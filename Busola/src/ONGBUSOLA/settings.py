e5ab9caa3f3450b3ba3551a88047469236e939ca
Yongobatohou <yongobatohou@gmail.com>
2025-12-16 14:10:36 +0100

    config git

--- Busola/src/ONGBUSOLA/settings.py
+++ Busola/src/ONGBUSOLA/settings.py
@@ -12,20 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "ONGBUSOLA" / ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t&!lm5y-8n#*@qe#_ks0x**t@a-!cd9%9kry@x-#jssn5r78e4'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
